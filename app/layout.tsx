52ed6c4cba65da281c5c14f0d53561b93edcf71f
fullstacksherpa <fullstacksherpa@gmail.com>
2024-01-10 20:58:48 -0800

    build: integrate sanity

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Fira_Code } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import { Provider } from "./utils/Provider";
 
 const inter = Inter({ subsets: ["latin"] });
+const firaCode = Fira_Code({ subsets: ["latin"] });
+
+
 
 export const metadata: Metadata = {
   title: "Blog",
@@ -14,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${firaCode.className} h-full bg-amber-50 text-indigo-950 dark:bg-slate-950 dark:text-amber-50 dark:selection:bg-purple-500`}>
         <Provider>
         <Navbar />
         <main className="mx-auto max-w-5xl px-6">{children}</main>
